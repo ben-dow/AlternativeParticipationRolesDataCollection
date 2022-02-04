55df1708a56fbb88818c74582897ba3a1a1e0e4f
Ben Dow <benjamindow1@gmail.com>
2022-02-03 20:24:16 -0500

    repo csv

---
+++ jsonReposToCSV.py
@@ -0,0 +1,23 @@
+import csv
+import json
+
+f = open("eligibleRepositories.txt", "r", encoding="utf-8")
+
+
+keys = []
+keysGot = False
+repos = []
+for r in f:
+    repo = json.loads(r)
+    repos.append(repo)
+    if not keys:
+        keys = repo['repo'].keys()
+        keysGot = True
+
+c = open('repos.csv', "w",newline='', encoding="utf-8")
+csvwriter = csv.DictWriter(c, fieldnames=keys)
+csvwriter.writeheader()
+
+for r in repos:
+    csvwriter.writerow(r['repo'])
+
